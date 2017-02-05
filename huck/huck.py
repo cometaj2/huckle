2b081a1219048a5fd14738fa2fabfa44dff75515
Jeff Michaud <cometaj2@comcast.net>
2017-02-05 14:13:25 -0800

    import haleasy and simple property fetch

--- huck/huck.py
+++ huck/huck.py
@@ -1,6 +1,10 @@
 import sys
+import json
+from haleasy import HALEasy
 
 def navigate(argv):
     if len(sys.argv) > 1:
         for x in sys.argv[1:]:
             print('Hello %s!' % x)
+            h = HALEasy('http://haltalk.herokuapp.com/')
+            print json.dumps(h.properties(), indent=4, sort_keys=True)
