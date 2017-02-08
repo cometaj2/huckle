c23fd06eb9d102e2547c9e63888057e812cc022f
Jeff Michaud <cometaj2@comcast.net>
2017-02-07 21:15:46 -0800

    clean up huck docs and implement standard docs for invoked clis

--- huck/huck.py
+++ huck/huck.py
@@ -4,19 +4,31 @@ import json
 
 from restnavigator import Navigator
 
-usage = """Usage:
-   
-- huck create <cliname> to alias into a new cli. Restarting the terminal
-  is required since we're using a .bash_profile alias.
+usage = """huck version 0.0.1.dev1
+
+desciption:
+    huck is a generic hypermedia client that provides a cli-like interaction with
+    hypermedia APIs that abide by a specific hypermedia API CLI pattern.
+
+synopsis:
+    huck <command> [parameter]
+
+commands:
+    create [cliname]
+    
+    This allows you to alias into a new cli. Restarting the terminal
+    is required since we're using a .bash_profile alias.
                    
-- huck cli <cliname> to invoke a cli. Note that the <cliname> alias created with
-  huck create <cliname> should be used instead, for brevity.
+    cli [cliname]
+    
+    Used to invoke a cli. Note that the [cliname] alias created with
+    huck create [cliname] should be used instead, for brevity.
 """
 
 def navigate(argv):
     h = Navigator.hal(config.url, apiname=config.cliname)
     if len(argv) == 1:
-      print json.dumps(h(), indent=4, sort_keys=True)
+        display_docs(h)
 
     length = len(argv[1:])
     for i, x in enumerate(argv[1:]):
@@ -26,7 +38,35 @@ def navigate(argv):
             break
 
         if i == length - 1:
-          print json.dumps(h(), indent=4, sort_keys=True)
+            display_docs(h)
+
+def display_docs(navigator):
+    print navigator()["name"] + " version " + navigator()["version"] + "\n"
+    print "description:"
+    print "    " + navigator()["description"] + "\n" 
+
+    print "synopsis:"
+    print "    " + navigator()["synopsis"]["description"] + "\n"
+    try:
+        navigator()["option"]
+        print "options:"
+        for i, x in enumerate(navigator()["option"]):
+            print "    " + navigator()["option"][i]["name"] + "\n"
+            print "    " + navigator()["option"][i]["description"] + "\n"
+    except:
+        pass
+
+    try:
+        navigator()["command"]
+        print "commands:"
+        for i, x in enumerate(navigator()["command"]):
+            print "    " + navigator()["command"][i]["name"] + "\n"
+            print "    " + navigator()["command"][i]["description"] + "\n"
+    except:
+        pass
+
+def pretty_json(navigator):
+    print json.dumps(navigator(), indent=4, sort_keys=True)
 
 def cli():
     if len(sys.argv) > 2:
