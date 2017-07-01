4f7c11213a60a6573845edf9b12700611d1af184
Jeff Michaud <cometaj2@comcast.net>
2017-07-01 16:00:58 -0700

    revert back to python 2.7 working version but retain 3.3 testing

--- huckle/__init__.py
+++ huckle/__init__.py
@@ -1,7 +1,5 @@
-from __future__ import absolute_import
-
-from .huckle import hutils
-from .huckle import config
+import hutils
+import config
 
 hutils.create_folder(config.dot_huckle)
 hutils.create_file(config.dot_huckle_profile)
