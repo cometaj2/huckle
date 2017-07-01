521dde6e1297a9a5586ab3992cb3bab2a0951a75
Jeff Michaud <cometaj2@comcast.net>
2017-07-01 16:34:06 -0700

    empty out __init__.py to avoid fudging the module namespace

--- huckle/__init__.py
+++
@@ -1,6 +0,0 @@
-import hutils
-import config
-
-hutils.create_folder(config.dot_huckle)
-hutils.create_file(config.dot_huckle_profile)
-hutils.create_file(config.dot_bash_profile)
--- huckle/__main__.py
+++ huckle/__main__.py
@@ -1,4 +1,10 @@
+import hutils
+import config
 import huckle
 
 def main():
+    hutils.create_folder(config.dot_huckle)
+    hutils.create_file(config.dot_huckle_profile)
+    hutils.create_file(config.dot_bash_profile)
+    
     huckle.cli()
