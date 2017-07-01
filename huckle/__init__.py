03a7a946c7df14f0cc3dfb6d7ca2d185d4323f41
Jeff Michaud <cometaj2@comcast.net>
2017-07-01 15:57:31 -0700

    future proof __init__.py to enable support for python 3.x

--- huckle/__init__.py
+++ huckle/__init__.py
@@ -1,5 +1,7 @@
-import hutils
-import config
+from __future__ import absolute_import
+
+from .huckle import hutils
+from .huckle import config
 
 hutils.create_folder(config.dot_huckle)
 hutils.create_file(config.dot_huckle_profile)
