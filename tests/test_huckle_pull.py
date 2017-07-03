8e273a313022d6ec3a0da098ee5ba61a34037ad7
Jeff Michaud <cometaj2@comcast.net>
2017-07-02 18:29:01 -0700

    explicitly set utf-8 on subprocess stdout in huckle pull test (python 3 compatibility)

--- tests/test_huckle_pull.py
+++ tests/test_huckle_pull.py
@@ -6,6 +6,6 @@ import os
 def test_function():
     cmd1 = 'tests/test_huckle_pull.sh'
     p1 = subprocess.Popen(cmd1.split(), stdout=subprocess.PIPE)
-    out, err = p1.communicate()
+    out, err = p1.communicate(str.encode('utf-8'))
 
-    assert('{\n  "hello": "world"\n}'.encode('utf-8') in out.encode('utf-8'))
+    assert('{\n  "hello": "world"\n}'.encode('utf-8') in out)
