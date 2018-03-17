65ecf252518f778c4721ad9c0e7cba25a997354b
Jeff Michaud <cometaj2@comcast.net>
2018-03-16 17:09:15 -0700

    fix hcli.io context root change in test

--- tests/test_huckle_install.py
+++ tests/test_huckle_install.py
@@ -7,7 +7,7 @@ def test_function():
     setup = """
     #!/bin/bash
 
-    huckle install https://hcli.io/hcli-webapp/cli/jsonf?command=jsonf
+    huckle install https://hcli.io/hcli/cli/jsonf?command=jsonf
     echo '{"hello":"world"}' | jsonf go
     """
 
