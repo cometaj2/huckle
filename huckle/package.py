4ac7bc7f26b89adcb027c4e52327df79cb6cd80c
Jeff Michaud <cometaj2@comcast.net>
2019-06-22 13:52:55 -0700

    drop to requests 2.21 to continue support for Python 3.4

--- huckle/package.py
+++ huckle/package.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 __version__ = "2.3.0"
 dependencies = ["restnavigator==1.0.1",
-                "requests[socks]==2.22.0",
+                "requests[socks]==2.21.0",
                 "configparser==3.5.0",
                 "requests_toolbelt==0.8.0"]
