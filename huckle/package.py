e78136396cf91d8ceb46b2cfd9327c58e0ce5d61
Jeff Michaud <cometaj2@comcast.net>
2019-06-29 15:10:36 -0700

    stay at verison 2.3.0

--- huckle/package.py
+++ huckle/package.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = "2.4.0"
+__version__ = "2.3.0"
 dependencies = ["restnavigator==1.0.1",
                 "requests[socks]==2.21.0",
                 "configparser==3.5.0",
