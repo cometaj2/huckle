e26dfd1fc60dae17ae361fa74961a88b9695dd94
Jeff Michaud <cometaj2@comcast.net>
2020-12-22 14:08:42 -0800

    fix package bug preventing huckle --version from working properly

--- huckle/package.py
+++ huckle/package.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = "2.5.5"
+__version__ = "2.5.6"
 dependencies = ["restnavigator==1.0.1",
                 "requests[socks]==2.21.0",
                 "configparser==3.5.0",
                 "requests_toolbelt==0.8.0",
-                "certifi>=2020.12.5"]
+                "certifi==2020.12.5"]
