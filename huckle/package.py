7f4ad0d4e9a8cad686b0db843fde93ce5e984c53
Jeff Michaud <cometaj2@comcast.net>
2023-09-03 18:08:28 -0700

    fix version discrepancy

--- huckle/package.py
+++ huckle/package.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-__version__ = "3.1.2"
+__version__ = "3.1.1"
 dependencies = ["restnavigator==1.0.1",
                 "requests[socks]==2.28.1",
                 "configparser==5.3.0",
