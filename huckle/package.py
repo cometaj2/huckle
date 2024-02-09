c38d58fa2f7110aea7e2402256864dfb8abfd2af
Jeff Michaud <cometaj2@comcast.net>
2024-02-09 13:58:07 -0800

    back to configparser 5.3.0

--- huckle/package.py
+++ huckle/package.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import, division, print_function
 __version__ = "3.3.2"
 dependencies = ["restnavigator==1.0.1",
                 "requests[socks]==2.28.1",
-                "configparser==6.0.0",
+                "configparser==5.3.0",
                 "certifi==2022.9.24"]
