d0444da9de9d5accde584c09c8e4fdd6a575c6d1
Jeff Michaud <cometaj2@comcast.net>
2024-04-25 20:22:41 -0700

    delayed import with __getattr__ to prevent pip dependency installation problems

--- huckle/__init__.py
+++ huckle/__init__.py
@@ -1,3 +1,10 @@
-from .huckle import cli, stdin
-
+# This code uses the __getattr__ magic method to dynamically import cli and stdin only
+# when they are accessed. Importing them directly at the top of __init__.py interfere with
+# the dependencies installation when huckle is installed via pip.
 __all__ = ['cli', 'stdin']
+
+def __getattr__(name):
+    if name in __all__:
+        from .huckle import cli, stdin
+        return locals()[name]
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
