5f2289b8bd2f30237afc29fb83c67ccc674a383a
Jeff Michaud <cometaj2@comcast.net>
2024-11-22 11:00:09 -0800

    update to latest version of certifi

--- huckle/package.py
+++ huckle/package.py
@@ -2,4 +2,4 @@ __version__ = "5.0.0"
 dependencies = ["restnavigator==1.0.1",
                 "requests[socks]==2.28.1",
                 "configparser==5.3.0",
-                "certifi==2022.9.24"]
+                "certifi==2024.8.30"]
